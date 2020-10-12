2e4661c290deb4142b58901d9a0fdd863ea16ffe
Nico Fischer <fischer.nico@outlook.com>
2020-10-12 20:56:35 +0200

    first tweaks

--- preinstall.sh
+++ preinstall.sh
@@ -4,73 +4,99 @@
 #     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
 #    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
 #   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
-#  Arch Linux Post Install Setup and Config
+#  Arch Linux Install Setup and Config with BTRFS on UEFI
 #-------------------------------------------------------------------------
 
 echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
+echo "Setting up mirrors for optimal download"
 echo "-------------------------------------------------"
+loadkeys de_CH-latin1
 timedatectl set-ntp true
+pacman -Sy --noconfirm
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-
+# Downloads the complete mirrorlist of https servers and sorts them
+# -e 's/^#Server/Server/' uncomments all servers | -e '/^#/d' deletes lines beginning with #
+curl -s "https://www.archlinux.org/mirrorlist/?country=AT&country=FR&country=DE&country=GR&country=IT&country=NL&country=ES&country=CH&country=GB&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 6 - >/etc/pacman.d/mirrorlist
 
 echo -e "\nInstalling prereqs...\n$HR"
 pacman -S --noconfirm gptfdisk btrfs-progs
 
 echo "-------------------------------------------------"
-echo "-------select your disk to format----------------"
+echo "-----------guided partitioning? (y)--------------"
 echo "-------------------------------------------------"
-lsblk
-echo "Please enter disk: (example /dev/sda)"
-read DISK
-echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
-echo "--------------------------------------"
+read guidedPartitioning
 
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+if [[ $guidedPartitioning =~ y ]]; then
+    echo "-------------------------------------------------"
+    echo "-------select your disk to format----------------"
+    echo "-------------------------------------------------"
+    lsblk
+    echo "Please enter disk: (example /dev/sda)\nCaution! This will erase the disk completly."
+    read DISK
+    echo "--------------------------------------"
+    echo -e "\nFormatting disk...\n$HR"
+    echo "--------------------------------------"
 
-# create partitions
-sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+    # disk prep for further information to partitioning: man sgdisk
+    sgdisk -Z ${DISK}         # zap all on disk
+    sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
-# set partition types
-sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+    # create partitions
+    sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+    sgdisk -n 2:0:0 ${DISK}      # partition 2 (Root), default start, remaining
 
-# label partitions
-sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"ROOT" ${DISK}
+    # set partition types
+    sgdisk -t 1:ef00 ${DISK}
+    sgdisk -t 2:8300 ${DISK}
 
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
+    # label partitions
+    sgdisk -c 1:"UEFISYS" ${DISK}
+    sgdisk -c 2:"ROOT" ${DISK}
 
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
-mkfs.ext4 -L "ROOT" "${DISK}2"
+    # make filesystems
+    echo -e "\nCreating Filesystems...\n$HR"
 
-# mount target
-mkdir /mnt
-mount -t ext4 "${DISK}2" /mnt
-mkdir /mnt/boot
-mkdir /mnt/boot/efi
-mount -t vfat "${DISK}1" /mnt/boot/
+    if [[ ${DISK} =~ nvme ]]; then
+        DISK=${DISK}p
+    fi
+
+    mkfs.fat -F32 -n "UEFISYS" "${DISK}1"
+    mkfs.btrfs -L "ROOT" "${DISK}2"
+
+    # mount target
+    mount "${DISK}2" /mnt
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@snapshots
+    umount /mnt
+    mount -o noatime,compress=lzo,space_cache,subvol=@ "${DISK}2" /mnt
+    mkdir -p /mnt/{boot,home,var,.snapshots}
+    mount -o noatime,compress=lzo,space_cache,subvol=@home "${DISK}2" /mnt/home
+    mount -o noatime,compress=lzo,space_cache,subvol=@var "${DISK}2" /mnt/var
+    mount -o noatime,compress=lzo,space_cache,subvol=@snapshots "${DISK}2" /mnt/.snapshots
+    mkdir /mnt/boot/efi
+    mount -t vfat "${DISK}1" /mnt/boot/
+else
+    echo "The script is now halted\nContinue by pressing Enter after partitioning and mounting everything.\nChange therefore the console with Ctrl + Alt + F2"
+    read waitingOnUser
+fi
 
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo --noconfirm --needed
-genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware vim nano sudo grub grub-btrfs snapper --noconfirm --needed
+genfstab -U /mnt >>/mnt/etc/fstab
+cat /mnt/etc/fstab
 arch-chroot /mnt
 
 echo "--------------------------------------"
 echo "-- Bootloader Systemd Installation  --"
 echo "--------------------------------------"
 bootctl install
-cat <<EOF > /boot/loader/entries/arch.conf
+cat <<EOF >/boot/loader/entries/arch.conf
 title Arch Linux  
 linux /vmlinuz-linux  
 initrd  /initramfs-linux.img  
